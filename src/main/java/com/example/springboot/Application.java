b26625c480ec33bd5fb76833d23414861d589569
anandmerry <62146389+anandmerry@users.noreply.github.com>
2020-05-10 22:41:29 +0530

    phase 2 branch testing

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-		System.out.println("Git Demo again");
+		System.out.println("Git Demo again - Phase2 branch");
 	}
 
 	@Bean
--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot - Git Demo again!";
+        return "Greetings from Spring Boot - Git Demo again - Phase 2 branch!";
     }
 
 }
